d776f42d732102e4365a7400a4144faa26ed1889
tisanyada <tisanyada@gmail.com>
2024-06-05 11:17:06 +0100

    updated application setup[10]

--- src/app/(pages)/(auth)/signin/page.tsx
+++ src/app/(pages)/(auth)/signin/page.tsx
@@ -4,7 +4,7 @@ import Link from 'next/link';
 import Image from 'next/image';
 import {toast} from 'react-hot-toast';
 import {signIn} from 'next-auth/react';
-import {useReducer, useState} from 'react';
+import {Suspense, useReducer, useState} from 'react';
 import {Button} from '@/components/ui/button';
 import {Separator} from '@/components/ui/separator';
 import {useGlobalStore} from '@/hooks/use-global-store';
@@ -109,109 +109,111 @@ const SignInPage = () => {
 	};
 
 	return (
-		<div className='w-full'>
-			<section className='h-[35vh] w-full bg-home flex flex-col items-center justify-center pt-10 md:pt-0'>
-				<h1 className='text-xl md:text-5xl font-medium text-white'>
-					Sign In
-				</h1>
-			</section>
-
-			<div className='flex flex-col justify-center items-center py-20'>
-				<form
-					autoComplete='off'
-					onSubmit={handleSubmit}
-					className='w-[90%] sm:w-[600px] py-10 px-4 sm:px-10 border rounded-lg shadow-md flex flex-col space-y-8'
-				>
-					<h1 className='text-center text-2xl font-semibold'>
+		<Suspense>
+			<div className='w-full'>
+				<section className='h-[35vh] w-full bg-home flex flex-col items-center justify-center pt-10 md:pt-0'>
+					<h1 className='text-xl md:text-5xl font-medium text-white'>
 						Sign In
 					</h1>
-					<div className='space-y-4'>
-						<FormTextInput
-							name='email'
-							padding='py-4 px-4'
-							value={formData.email}
-							handleChange={handleChange}
-							placeHolder='Email'
-							classes='w-full text-sm placeholder:text-sm border focus:border-slate-500 rounded'
-						/>
-						<FormPasswordInput
-							name='password'
-							padding='py-4 px-4'
-							value={formData.password}
-							handleChange={handleChange}
-							placeHolder='Password'
-							classes='w-full text-sm placeholder:text-sm border focus:border-slate-500 rounded'
-						/>
+				</section>
 
-						<div className='flex justify-between items-center'>
-							<div className='space-x-3 flex items-center'>
-								{' '}
-								<input
-									type='checkbox'
-									name='remember-me'
-									id=''
-								/>
-								<p className='text-sm'>Remember me</p>
+				<div className='flex flex-col justify-center items-center py-20'>
+					<form
+						autoComplete='off'
+						onSubmit={handleSubmit}
+						className='w-[90%] sm:w-[600px] py-10 px-4 sm:px-10 border rounded-lg shadow-md flex flex-col space-y-8'
+					>
+						<h1 className='text-center text-2xl font-semibold'>
+							Sign In
+						</h1>
+						<div className='space-y-4'>
+							<FormTextInput
+								name='email'
+								padding='py-4 px-4'
+								value={formData.email}
+								handleChange={handleChange}
+								placeHolder='Email'
+								classes='w-full text-sm placeholder:text-sm border focus:border-slate-500 rounded'
+							/>
+							<FormPasswordInput
+								name='password'
+								padding='py-4 px-4'
+								value={formData.password}
+								handleChange={handleChange}
+								placeHolder='Password'
+								classes='w-full text-sm placeholder:text-sm border focus:border-slate-500 rounded'
+							/>
+
+							<div className='flex justify-between items-center'>
+								<div className='space-x-3 flex items-center'>
+									{' '}
+									<input
+										type='checkbox'
+										name='remember-me'
+										id=''
+									/>
+									<p className='text-sm'>Remember me</p>
+								</div>
+								<Link
+									href='/forgot-password'
+									className='text-sm text-green-600'
+								>
+									Forgot password?
+								</Link>
 							</div>
-							<Link
-								href='/forgot-password'
-								className='text-sm text-green-600'
-							>
-								Forgot password?
-							</Link>
-						</div>
 
-						{loading === true ? (
+							{loading === true ? (
+								<Button
+									type='button'
+									className='bg-green-700 text-white h-12 hover:bg-green-700 w-full rounded-full py-4 cursor-default'
+								>
+									<ButtonLoader />
+								</Button>
+							) : (
+								<Button
+									type='submit'
+									className='bg-green-600 text-white h-12 hover:bg-green-700 w-full rounded-full py-4'
+								>
+									Sign In
+								</Button>
+							)}
+
+							<div className='flex items-center justify-between space-x-3'>
+								<Separator className='w-[43%]' />
+								<span>Or</span>
+								<Separator className='w-[43%]' />
+							</div>
+
 							<Button
 								type='button'
-								className='bg-green-700 text-white h-12 hover:bg-green-700 w-full rounded-full py-4 cursor-default'
+								variant={'outline'}
+								onClick={() => signIn('google')}
+								className='flex items-center gap-x-4 h-12 justify-center w-full rounded-full py-4'
 							>
-								<ButtonLoader />
+								<Image
+									unoptimized={true}
+									alt='google icon'
+									src={'/icon_google.svg'}
+									width={30}
+									height={30}
+								/>
+								<p>Continue with Google</p>
 							</Button>
-						) : (
-							<Button
-								type='submit'
-								className='bg-green-600 text-white h-12 hover:bg-green-700 w-full rounded-full py-4'
-							>
-								Sign In
-							</Button>
-						)}
 
-						<div className='flex items-center justify-between space-x-3'>
-							<Separator className='w-[43%]' />
-							<span>Or</span>
-							<Separator className='w-[43%]' />
+							<div className='flex justify-center mt-5'>
+								<Link
+									href='/signup'
+									className='text-sm text-center mx-auto'
+								>
+									Don't have an account?{' '}
+									<span className='text-main'>Register</span>
+								</Link>
+							</div>
 						</div>
-
-						<Button
-							type='button'
-							variant={'outline'}
-							onClick={() => signIn('google')}
-							className='flex items-center gap-x-4 h-12 justify-center w-full rounded-full py-4'
-						>
-							<Image
-								unoptimized={true}
-								alt='google icon'
-								src={'/icon_google.svg'}
-								width={30}
-								height={30}
-							/>
-							<p>Continue with Google</p>
-						</Button>
-
-						<div className='flex justify-center mt-5'>
-							<Link
-								href='/signup'
-								className='text-sm text-center mx-auto'
-							>
-								Don't have an account?{' '}
-								<span className='text-main'>Register</span>
-							</Link>
-						</div>
-					</div>
-				</form>
+					</form>
+				</div>
 			</div>
-		</div>
+		</Suspense>
 	);
 };
 
