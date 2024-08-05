c08f74049de4214c15375034ed3adeebef018999
tisanyada <tisanyada@gmail.com>
2024-08-05 10:05:05 +0100

    updated dashboard content share link config

--- src/app/(pages)/(public)/(routes)/account/components/dashboard/dashboard-content.tsx
+++ src/app/(pages)/(public)/(routes)/account/components/dashboard/dashboard-content.tsx
@@ -7,74 +7,22 @@ import {
 	WhatsappShareButton,
 } from 'react-share';
 import Image from 'next/image';
+import {useEffect} from 'react';
 import {Copy} from 'lucide-react';
 import {toast} from 'react-hot-toast';
 import axios, {AxiosError} from 'axios';
-import {useEffect, useState} from 'react';
-import {Button} from '@/components/ui/button';
-import {DataTable} from '@/components/ui/data-table';
 import {useGlobalStore} from '@/hooks/use-global-store';
 import {CopyToClipboard} from 'react-copy-to-clipboard';
-import ButtonLoader from '@/components/loader/button-loader';
-import {RecentOrderColumn, columns} from './tables/recent-orders-columns';
-import {Vendor} from '@/types/types';
 import {formatVendorSlug} from '@/utils/slug.formatter';
 
-const RecentOrders: RecentOrderColumn[] = [
-	{
-		id: '1',
-		productId: '#123',
-		date: '8 Sep, 2023',
-		total: '₦13,500 (2 Products)',
-		status: 'Processing',
-	},
-	{
-		id: '2',
-		productId: '#259',
-		date: '8 Sep, 2023',
-		total: '₦17,500 (2 Products)',
-		status: 'Delivered',
-	},
-	{
-		id: '3',
-		productId: '#260',
-		date: '8 Oct, 2023',
-		total: '₦14,500 (2 Products)',
-		status: 'Delivered',
-	},
-	{
-		id: '4',
-		productId: '#460',
-		date: '8 Sep, 2023',
-		total: '₦13,500 (2 Products)',
-		status: 'Processing',
-	},
-	{
-		id: '5',
-		productId: '#782',
-		date: '8 Sep, 2023',
-		total: '₦17,500 (2 Products)',
-		status: 'Delivered',
-	},
-	{
-		id: '6',
-		productId: '#030',
-		date: '8 Oct, 2023',
-		total: '₦14,500 (2 Products)',
-		status: 'Delivered',
-	},
-];
-
-interface DashboardContentProps {
-	// user: User | null;
-	// updateTab: Dispatch<SetStateAction<Tab>>;
-}
-
-const DashboardContent = ({}: DashboardContentProps) => {
-	const {user, vendor, updateUser, updateVendor, updateCurrentAccountTab} =
-		useGlobalStore();
-
-	const [loading, setLoading] = useState<boolean>(false);
+const DashboardContent = () => {
+	const {
+		user,
+		vendorProfile,
+		updateUser,
+		updateVendorProfile,
+		updateCurrentAccountTab,
+	} = useGlobalStore();
 
 	const fetchVendorProfile = async () => {
 		try {
@@ -89,7 +37,7 @@ const DashboardContent = ({}: DashboardContentProps) => {
 
 			// console.log('[DATA] ::  ', data);
 
-			updateVendor(data.data);
+			updateVendorProfile(data.data);
 		} catch (error) {
 			const _error = error as AxiosError;
 
@@ -101,45 +49,6 @@ const DashboardContent = ({}: DashboardContentProps) => {
 		fetchVendorProfile();
 	}, []);
 
-	const handleUpdateUserRole = async (role: string) => {
-		try {
-			setLoading(true);
-
-			// console.log('[UPDATE-USER-ROLE-PAYLOAD] :: ', user);
-			// console.log('[UPDATE-USER-ROLE-PAYLOAD] :: ', role);
-
-			const {data} = await axios.patch(
-				`${process.env.NEXT_PUBLIC_API_URL}/auth/update-user-role`,
-				{role},
-				{
-					headers: {
-						Authorization: user?.accessToken,
-					},
-				}
-			);
-
-			const cookieUpdate = await axios.patch(
-				'/api/auth/update-cookies',
-				data.data
-			);
-
-			setLoading(false);
-
-			// // console.log('[USER-ROLE] :: ', cookieUpdate.data);
-			await updateUser(cookieUpdate.data);
-
-			toast.success('User role updated!');
-		} catch (error) {
-			setLoading(false);
-
-			const _error = error as AxiosError;
-
-			// console.log('[UPDATE-USER-ROLE-ERROR]', _error);
-
-			toast.error('Error');
-		}
-	};
-
 	return (
 		<div className='w-full md:w-[78%] flex flex-col gap-5'>
 			<div className='flex flex-col md:flex-row items-center justify-between w-full'>
@@ -147,14 +56,11 @@ const DashboardContent = ({}: DashboardContentProps) => {
 					<div className='text-center'>
 						<div className='h-[150px] w-[150px] rounded-full border relative'>
 							<Image
-								alt='image'
-								// width={150}
-								// height={150}
 								fill
+								alt='image'
 								unoptimized={true}
-								// src={'/user__1.svg'}
-								className='object-cover rounded-full h-full w-full'
 								src={user?.avatar ?? '/user__1.svg'}
+								className='object-cover rounded-full h-full w-full'
 							/>
 						</div>
 
@@ -166,56 +72,62 @@ const DashboardContent = ({}: DashboardContentProps) => {
 						</p>
 					</div>
 
-					{user?.role === 'FARMER' && (
-						<div className='flex items-center space-x-4'>
-							<p
-								// onClick={() => updateCurrentAccountTab('Settings')}
-								className='text-sm'
-							>
-								Share Profile:
-							</p>
+					{user?.role === 'FARMER' &&
+						vendorProfile?.slug?.length! > 1 && (
+							<div className='flex items-center space-x-4'>
+								<p
+									// onClick={() => updateCurrentAccountTab('Settings')}
+									className='text-sm'
+								>
+									Share Profile:
+								</p>
 
-							<div className='flex space-x-2'>
-								<WhatsappShareButton
-									url={`https://animaff.com/store/${formatVendorSlug(
-										vendor!
-									)}`}
-									title='Check out my online store on Animaff: '
-								>
-									<WhatsappIcon size={25} round />
-								</WhatsappShareButton>
-								<FacebookShareButton
-									url={`https://animaff.com/store/${formatVendorSlug(
-										vendor!
-									)}`}
-									title='Check out my online store on Animaff: '
-								>
-									<FacebookIcon size={25} round />
-								</FacebookShareButton>
-								<TwitterShareButton
-									url={`https://animaff.com/store/${formatVendorSlug(
-										vendor!
-									)}`}
-									title='Check out my online store on Animaff: '
-								>
-									<TwitterIcon size={25} round />
-								</TwitterShareButton>
+								<div className='flex space-x-2'>
+									<WhatsappShareButton
+										url={`https://animaff.com/store/${formatVendorSlug(
+											vendorProfile!
+										)}`}
+										title={`Check out my min-website/online store ${vendorProfile?.name} on Animaff: `}
+									>
+										<WhatsappIcon size={25} round />
+									</WhatsappShareButton>
+									<FacebookShareButton
+										url={`https://animaff.com/store/${formatVendorSlug(
+											vendorProfile!
+										)}`}
+										title={`Check out my min-website/online store ${vendorProfile?.name} on Animaff: `}
+									>
+										<FacebookIcon size={25} round />
+									</FacebookShareButton>
+									<TwitterShareButton
+										url={`https://animaff.com/store/${formatVendorSlug(
+											vendorProfile!
+										)}`}
+										title={`Check out my min-website/online store ${vendorProfile?.name} on Animaff: `}
+									>
+										<TwitterIcon size={25} round />
+									</TwitterShareButton>
 
-								<CopyToClipboard
-									onCopy={(text: string, result: boolean) => {
-										toast.success('Copied to clipboard');
-									}}
-									text={`https://animaff.com/store/${formatVendorSlug(
-										vendor!
-									)}`}
-								>
-									<div className='rounded-full border border-slate-400 h-7 w-7 flex items-center justify-center cursor-pointer'>
-										<Copy className='h-4 w-4' />
-									</div>
-								</CopyToClipboard>
+									<CopyToClipboard
+										onCopy={(
+											text: string,
+											result: boolean
+										) => {
+											toast.success(
+												'Copied to clipboard'
+											);
+										}}
+										text={`https://animaff.com/store/${formatVendorSlug(
+											vendorProfile!
+										)}`}
+									>
+										<div className='rounded-full border border-slate-400 h-7 w-7 flex items-center justify-center cursor-pointer'>
+											<Copy className='h-4 w-4' />
+										</div>
+									</CopyToClipboard>
+								</div>
 							</div>
-						</div>
-					)}
+						)}
 				</div>
 				<div className='flex flex-col items-start justify-between h-[350px] w-full md:w-[45%]'>
 					<div className='p-5 flex flex-col items-start w-full h-full justify-between border rounded-lg'>
