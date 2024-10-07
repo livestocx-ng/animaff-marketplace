b4b4514668bd1eda1b0f1c269619703eb67bf159
tisanyada <tisanyada@gmail.com>
2024-10-07 09:18:07 +0100

    updated sitemap setup

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,19 +1,19 @@
-import {fetchSiteProducts} from '@/lib/siteMapData';
-import {formatProductSlug} from '@/utils/slug.formatter';
+import {fetchSiteBlogs} from '@/lib/siteMapData';
+import {formatBlogSlug, formatProductSlug} from '@/utils/slug.formatter';
 
 export default async function sitemap() {
 	const baseUrl = 'https://animaff.com';
 
-	// const products = await fetchSiteProducts();
+	const blogs = await fetchSiteBlogs();
 
-	// const productUrls = products?.map((product) => {
-	// 	return {
-	// 		url: `${baseUrl}/marketplace/products/${formatProductSlug(
-	// 			product!
-	// 		)}`,
-	// 		lastModified: new Date(product.createdAt),
-	// 	};
-	// });
+	const blogUrls = blogs?.map((blog) => {
+		return {
+			url: `${baseUrl}/blog/${formatBlogSlug(blog!)}`,
+			lastModified: new Date(),
+		};
+	});
+
+	console.log('[BLOG-URLS] :: ', blogUrls);
 
 	return [
 		{
@@ -21,9 +21,18 @@ export default async function sitemap() {
 			lastModified: new Date(),
 		},
 		{
+			url: `${baseUrl}/enterprise`,
+			lastModified: new Date(),
+		},
+		{
+			url: `${baseUrl}/pricing`,
+			lastModified: new Date(),
+		},
+		{
 			url: `${baseUrl}/blog`,
 			lastModified: new Date(),
 		},
+		...blogUrls,
 		{
 			url: `${baseUrl}/privacy-policy`,
 			lastModified: new Date(),
--- src/lib/siteMapData.ts
+++ src/lib/siteMapData.ts
@@ -1,6 +1,25 @@
-import {Product} from '@/types/types';
+import {Blog, Product} from '@/types/types';
 import axios from 'axios';
 import {NextResponse} from 'next/server';
+
+export async function fetchSiteBlogs(): Promise<Blog[]> {
+	const response = await fetch(
+		`${process.env.NEXT_PUBLIC_API_URL}/blog/fetch-all`,
+		{
+			cache: 'force-cache',
+			next: {
+				revalidate: 3600,
+			},
+		}
+	);
+
+	if (!response.ok) {
+		throw new Error('Failed to fetch blogs');
+	}
+
+	const data = await response.json();
+	return data.data;
+}
 
 export async function fetchSiteProducts(): Promise<Product[]> {
 	const {data} = await axios.get(
